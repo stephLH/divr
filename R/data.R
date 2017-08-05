f5dd4cfe0dfd3bdfd0b39db13369e9c18a3cc91e
Stephane Le Henanff <github@le-henanff.fr>
2017-08-05 13:01:58 +0200

    Ajout d'une fonction data() permettant de mettre à jour les données depuis une base Access embarquée

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' data
+#'
+#' @export
+data <- function() {
+
+  data_lib_num_mois <- importr::importer_table_access("libelle_num_mois", paste0(racine_packages, "divr/Tables_ref.accdb"))
+  save("data_lib_num_mois", file = paste0(racine_packages, "divr/data/data_lib_num_mois.RData"))
+
+}
