d22b6141cc28f0bba446272fe2a96ad8a51ea9d3
Stephane Le Henanff <github@le-henanff.fr>
2017-08-12 10:01:36 +0200

    Ajout de la fonction dependances_package

---
+++ R/dev.R
@@ -0,0 +1,22 @@
+#' Obtenir les dependances d'un packages
+#'
+#' Obtenir les dépendances d'un packages.
+#'
+#' @param repertoire Le répertoire racine du package.
+#'
+#' @return Un vecteur de dépendances.
+#'
+#' @examples
+#'
+#' @export
+dependances_package <- function(repertoire) {
+
+  dependances <- list.files(repertoire, pattern = "\\.R$", full.names = TRUE, recursive = TRUE) %>%
+    purrr::map( ~ readr::read_lines(.)) %>%
+    unlist() %>%
+    stringr::str_match_all("([A-z]+)::") %>%
+    purrr::map_df( ~ dplyr::as_tibble(.)) %>%
+    dplyr::pull(V2) %>% unique()
+
+  return(dependances)
+}
