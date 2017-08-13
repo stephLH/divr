ea4ac218abd14156fbbd12f6d8156aa9ba6398a9
Stephane Le Henanff <github@le-henanff.fr>
2017-08-13 16:16:13 +0200

    dependances_package : correction de la regex pour détecter les noms de package

--- R/dev.R
+++ R/dev.R
@@ -14,7 +14,7 @@ dependances_package <- function(repertoire = ".") {
   dependances <- list.files(repertoire, pattern = "\\.R$", full.names = TRUE, recursive = TRUE) %>%
     lapply(readr::read_lines) %>%
     unlist() %>%
-    stringr::str_match_all("([A-z\\.]+)::") %>%
+    stringr::str_match_all("([[:alpha:]\\.]+)::") %>%
     purrr::map_df( ~ dplyr::as_tibble(.)) %>%
     dplyr::pull(V2) %>% unique() %>% sort()
 
