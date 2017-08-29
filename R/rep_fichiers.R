384dc466dc017956098cc51b0510ca7b6bc79885
Stephane Le Henanff <github@le-henanff.fr>
2017-08-29 19:44:14 +0200

    extraction zip : par défaut dans le répertoire de l'archive

--- R/rep_fichiers.R
+++ R/rep_fichiers.R
@@ -107,7 +107,7 @@ supprimer_repertoire <- function(repertoire) {
 #'
 #' @param archive_zip Chemin du fichier zip dont le contenu est extrait.
 #' @param regex_fichier Expression régulière pour filtrer les fichiers à extraire.
-#' @param repertoire_sortie Chemin du répertoire vers lequel est réalisée l'extraction (par défaut, le répertoire de travail).
+#' @param repertoire_sortie Chemin du répertoire vers lequel est réalisée l'extraction (par défaut, le répertoire de l'archive zip).
 #' @param supprimer_zip \code{TRUE}: l'archive zip est supprimée; \code{FALSE}: l'archive zip n'est pas supprimée..
 #'
 #' @examples
@@ -115,11 +115,15 @@ supprimer_repertoire <- function(repertoire) {
 #' divr::extraire_fichiers_zip("archive.zip", regex_fichier = "pdf$", repertoire_sortie = "test_zip")
 #'
 #' @export
-extraire_fichiers_zip <- function(archive_zip, regex_fichier = NULL, repertoire_sortie = ".", supprimer_zip = FALSE) {
+extraire_fichiers_zip <- function(archive_zip, regex_fichier = NULL, repertoire_sortie = NULL, supprimer_zip = FALSE) {
 
   fichiers <- unzip(archive_zip, list = TRUE)[["Name"]]
 
   if (!is.null(regex_fichier)) fichiers <- fichiers[stringr::str_detect(fichiers, regex_fichier)]
+
+  if (is.null(repertoire_sortie)) {
+    repertoire_sortie <- stringr::str_match(archive_zip, "(.+)/")[, 2]
+  }
 
   unzip(archive_zip, fichiers, exdir = repertoire_sortie)
 
@@ -135,24 +139,20 @@ extraire_fichiers_zip <- function(archive_zip, regex_fichier = NULL, repertoire_
 #'
 #' @param chemin Répertoire dans lequel sont cherchés (récursif) les archives zip dont le contenu est extrait.
 #' @param regex_fichier Expression régulière pour filtrer les fichiers à extraire.
-#' @param repertoire_sortie Chemin du répertoire vers lequel est réalisée l'extraction (par défaut, le répertoire de travail).
 #' @param return_tibble Retourne ou non un data frame fournissant la correspondance entre les archives zip initiales et l'emplacement des fichiers extraits.
+#' @param paralleliser \code{TRUE}, extraction parallelisée des archives zip.
 #'
 #' @examples
 #' # Extraire les fichiers pdf stockés dans les archives zip du répertoire "Chemin/vers/un/répartoire"
 #' divr::extraire_masse_zip("Chemin/vers/un/répartoire", regex_fichier = "pdf$", repertoire_sortie = "test_zip")
 #'
 #' @export
-extraire_masse_zip <- function(chemin, regex_fichier, repertoire_sortie = ".", vider_repertoire_sortie = TRUE, return_tibble = TRUE, paralleliser = TRUE) {
+extraire_masse_zip <- function(chemin, regex_fichier, return_tibble = TRUE, paralleliser = FALSE) {
 
   if (paralleliser == TRUE) {
     clusters <- divr::initialiser_cluster()
   } else {
     clusters <- NULL
-  }
-
-  if (vider_repertoire_sortie == TRUE) {
-    vider_repertoire(repertoire_sortie)
   }
 
   archives_zip <- dplyr::tibble(archive_zip = list.files(chemin, pattern = "\\.zip$", recursive = TRUE, full.names = TRUE)) %>%
@@ -164,17 +164,29 @@ extraire_masse_zip <- function(chemin, regex_fichier, repertoire_sortie = ".", v
     dplyr::inner_join(archives_zip, ., by = "num_archive") %>%
     dplyr::select(-num_archive)
 
-  message("Décompression zip des fichiers dans le dossier \"", repertoire_sortie, "\":")
-  pbapply::pblapply(archives_zip$archive_zip, extraire_fichiers_zip, regex_fichier = regex_fichier, repertoire_sortie = repertoire_sortie, cl = clusters) %>%
+  message("Décompression zip des fichiers...")
+
+  archives_zip %>%
+    split(1:nrow(.)) %>%
+    pbapply::pblapply(function(ligne) {
+
+      divr::extraire_fichiers_zip(ligne$archive_zip,
+                                  regex_fichier = regex_fichier)
+
+    }, cl = clusters) %>%
     invisible()
 
   if (paralleliser == TRUE) {
     divr::stopper_cluster(clusters)
   }
 
-  archives_zip <- dplyr::mutate(archives_zip, fichier = paste0(repertoire_sortie, "/", fichier))
+  if (return_tibble == TRUE) {
+    archives_zip <- archives_zip %>%
+      dplyr::mutate(repertoire_sortie = stringr::str_match(archive_zip, "(.+)/")[, 2],
+                    fichier = paste0(repertoire_sortie, "/", fichier))
+    return(archives_zip)
+  }
 
-  if (return_tibble == TRUE) return(archives_zip)
 }
 
 #' Concatener des fichiers
