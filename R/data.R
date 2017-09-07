e25ab49af34dc238e3b1e50d7567590e4908ddfd
Stéphane Le Hénanff <slehenanff@ups-tlse.fr>
2017-09-07 13:41:40 +0200

    gitignore : ajout de la base Access

--- R/data.R
+++ R/data.R
@@ -4,7 +4,7 @@
 #' @keywords internal
 data <- function() {
 
-  lib_num_mois <- importr::importer_table_access("libelle_num_mois", paste0(racine_packages, "divr/data/Tables_ref.accdb"))
+  lib_num_mois <- importr::importer_table_access("libelle_num_mois", paste0(racine_packages, "divr/Tables_ref.accdb"))
   save("lib_num_mois", file = paste0(racine_packages, "divr/data/lib_num_mois.RData"))
 
 }
