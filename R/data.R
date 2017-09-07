f99dfc4b814eb344e5e8b0cfb3f9c457594c8816
Stéphane Le Hénanff <slehenanff@ups-tlse.fr>
2017-09-07 13:43:03 +0200

    fonction data : même nom de fonction que le package utils -> generer_data

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #'
 #' @export
 #' @keywords internal
-data <- function() {
+generer_data <- function() {
 
   lib_num_mois <- importr::importer_table_access("libelle_num_mois", paste0(racine_packages, "divr/Tables_ref.accdb"))
   save("lib_num_mois", file = paste0(racine_packages, "divr/data/lib_num_mois.RData"))
