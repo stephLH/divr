8baf95fe5a51fbcdd78ee49b831f9b59bb4d773b
Stéphane Le Hénanff <github@le-henanff.fr>
2018-06-05 09:54:33 +0200

    Ajout de la fonction extraire_nom_fichier

--- R/rep_fichiers.R
+++ R/rep_fichiers.R
@@ -74,6 +74,25 @@ extension_fichier <- function(fichier) {
   return(extension_fichier)
 }
 
+#' Extraire le fichier a partir du chemin complet
+#'
+#' Extraire le fichier à partir du chemin complet.
+#'
+#' @param chemin_fichier Chemin complet dont le fichier est extrait.
+#'
+#' @return Le nom du fichier.
+#'
+#' @examples
+#' divr::extraire_nom_fichier(c("Dossier/fichier_excel.xlsx", "Dossier2/fichier_word.docx"))
+#'
+#' @export
+extraire_nom_fichier <- function(chemin_fichier) {
+
+  nom_fichier <- stringr::str_match(chemin_fichier, "([^\\/]+?)$")[, 2]
+
+  return(nom_fichier)
+}
+
 #' Supprimer un repertoire
 #'
 #' Supprimer un repertoire.
