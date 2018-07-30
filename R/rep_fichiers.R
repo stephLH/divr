794543155e9770da1c1a48b01af5e9bde63a81d2
Stephane Le Henanff <github@le-henanff.fr>
2018-07-30 20:41:17 +0200

    Anglicisation de extraire_fichiers_zip -> zip_extract_file

--- R/rep_fichiers.R
+++ R/rep_fichiers.R
@@ -1,35 +1,34 @@
-#' Extraire des fichiers d'une archive zip
+#' Extract files from a zip.
 #'
-#' Extraire des fichiers d'une archive zip.
+#' Files within the zip file can be selected with the pattern argument.
 #'
-#' @param archive_zip Chemin du fichier zip dont le contenu est extrait.
-#' @param regex_fichier Expression régulière pour filtrer les fichiers à extraire.
-#' @param repertoire_sortie Chemin du répertoire vers lequel est réalisée l'extraction (par défaut, le répertoire de l'archive zip).
-#' @param supprimer_zip \code{TRUE}: l'archive zip est supprimée; \code{FALSE}: l'archive zip n'est pas supprimée..
+#' @param zip_file Path to the zip file.
+#' @param pattern an optional regular expression. Only file names which match the regular expression will be extracted.
+#' @param exdir The directory to extract files to. It will be created if necessary.
+#' @param remove_zip If \code{TRUE}, the zip file is removed.
 #'
 #' @examples
-#' # Extraire les fichiers pdf d'une archive
-#' divr::extraire_fichiers_zip("archive.zip", regex_fichier = "pdf$", repertoire_sortie = "test_zip")
+#' divr::zip_extract_file("archive.zip", pattern = "pdf$", exdir = "test_zip")
 #'
 #' @export
-extraire_fichiers_zip <- function(archive_zip, regex_fichier = NULL, repertoire_sortie = NULL, supprimer_zip = FALSE) {
+zip_extract_file <- function(zip_file, pattern = NULL, exdir = NULL, remove_zip = FALSE) {
 
-  if (!file.exists(archive_zip)) {
-    stop("L'archive \"", archive_zip,"\" n'existe pas.", call. = FALSE)
+  if (!file.exists(zip_file)) {
+    stop("The zip file \"", zip_file,"\" does not exist", call. = FALSE)
   }
 
-  fichiers <- unzip(archive_zip, list = TRUE)[["Name"]]
+  files <- unzip(zip_file, list = TRUE)[["Name"]]
 
-  if (!is.null(regex_fichier)) fichiers <- fichiers[stringr::str_detect(fichiers, regex_fichier)]
+  if (!is.null(pattern)) files <- files[stringr::str_detect(files, pattern)]
 
-  if (is.null(repertoire_sortie)) {
-    repertoire_sortie <- stringr::str_match(archive_zip, "(.+)/")[, 2]
+  if (is.null(exdir)) {
+    exdir <- stringr::str_match(zip_file, "(.+)/")[, 2]
   }
 
-  unzip(archive_zip, fichiers, exdir = repertoire_sortie)
+  unzip(zip_file, files, exdir = exdir)
 
-  if (supprimer_zip == TRUE) {
-    file.remove(archive_zip) %>%
+  if (remove_zip == TRUE) {
+    file.remove(zip_file) %>%
       invisible()
   }
 }
@@ -96,7 +95,7 @@ extraire_masse_zip <- function(chemin, regex_fichier, regex_zip = "\\.zip$", n_f
     split(1:nrow(.)) %>%
     pbapply::pblapply(function(ligne) {
 
-      divr::extraire_fichiers_zip(ligne$archive_zip,
+      divr::zip_extract_file(ligne$archive_zip,
                                   regex_fichier = regex_fichier)
 
     }, cl = clusters)
