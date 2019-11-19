4049e80c70e4bde55c2712b7de69864cebd39862
Stéphane Le Hénanff <github@le-henanff.fr>
2019-11-19 09:12:13 +0100

    chrome_print_no_gpu: rename from convert_html_to_pdf

--- R/convert.R
+++ R/convert.R
@@ -5,22 +5,15 @@
 #' @param  remove_html \dots
 #'
 #' @export
-convert_html_to_pdf <- function(html_file, pdf_file = NULL, remove_html = FALSE) {
+chrome_print_no_gpu <- function(html_file, pdf_file = NULL, remove_html = FALSE) {
 
   if (is.null(pdf_file)) {
     pdf_file <- stringr::str_replace(html_file, "\\.html$", ".pdf")
   }
 
-  if (Sys.info()[["sysname"]] == "Windows") {
+  chrome_path <- pagedown::find_chrome()
 
-    pagedown::chrome_print(html_file, pdf_file)
-
-  } else if (Sys.info()[["sysname"]] == "Linux") {
-
-    chrome_path <- pagedown::find_chrome()
-
-    system(glue::glue("\"{chrome_path}\" --headless --disable-gpu --print-to-pdf=\"{pdf_file}\" \"{html_file}\""), intern = TRUE)
-  }
+  system(glue::glue("\"{chrome_path}\" --headless --disable-gpu --print-to-pdf=\"{pdf_file}\" \"{html_file}\""), intern = TRUE)
 
   if (remove_html == TRUE) {
     suppression <- file.remove(html_file)
