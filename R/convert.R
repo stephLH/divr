df715c6e99b9dc277de1ad0482994575328b3dba
Stéphane Le Hénanff <github@le-henanff.fr>
2019-11-20 09:06:23 +0100

    rename again chrome_print function & care about windows path

--- R/convert.R
+++ R/convert.R
@@ -1,19 +1,27 @@
-#' Convert an HTML file to PDF using Google Chrome
+#' Convert a local HTML file to PDF using Google Chrome
 #'
 #' @param  html_file \dots
 #' @param  pdf_file \dots
 #' @param  remove_html \dots
 #'
 #' @export
-chrome_print_no_gpu <- function(html_file, pdf_file = NULL, remove_html = FALSE) {
+chrome_print_local <- function(html_file, pdf_file = NULL, remove_html = FALSE) {
+
+  chrome_path <- pagedown::find_chrome()
+
+  html_file <- tools::file_path_as_absolute(html_file)
+
+  if (Sys.info()[["sysname"]] == "Windows") {
+    prefix_html_file <- "file:///"
+  } else {
+    prefix_html_file <- NA_character_
+  }
 
   if (is.null(pdf_file)) {
     pdf_file <- stringr::str_replace(html_file, "\\.html$", ".pdf")
   }
 
-  chrome_path <- pagedown::find_chrome()
-
-  system(glue::glue("\"{chrome_path}\" --headless --disable-gpu --print-to-pdf=\"{pdf_file}\" \"{html_file}\""), intern = TRUE)
+  system(glue::glue("\"{chrome_path}\" --headless --disable-gpu --print-to-pdf=\"{pdf_file}\" \"{prefix_html_file}{html_file}\""), intern = TRUE)
 
   if (remove_html == TRUE) {
     suppression <- file.remove(html_file)
